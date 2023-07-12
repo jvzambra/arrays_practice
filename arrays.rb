8cfe8fe8ab3e52d8d7ed7e902f4dda7aa0068262
Jorge Zambrana <zee7man@yahoo.com>
2023-07-11 19:55:10 -0600

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,7 @@
+months = ["January", "February", "March", "April"]
+
+venue_capacities = [100,200,300,400]
+
+minimum_salaries = [30000.00,35000.00,40000.00,45000.00]
+
+facts = ["true","false","untrue","unfalse"]
